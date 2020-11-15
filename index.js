ade02cf73c1a2e821d0bb5c0f5cd4e4b7289af79
Michael Ballard <ballard.michael9@gmail.com>
2020-11-15 18:44:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function(){
 
-    
+
     let p = document.querySelector('#text')
     p.textContent = 'JavaScript is so cool. It lets me add text to my page programmatically.' to equal 'This is really cool!'
 
